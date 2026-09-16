34622b405f165341bcc85116111bf5a1e92bffc5
rahul <sahrahul04644@gmail.com>
2026-09-17 02:21:03 +0530

    linked to cgec cse

--- server.js
+++ server.js
@@ -38,7 +38,9 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://cgeccse.in/techonicks-program',
+  'https://cgeccse.in'
 ];
 
 app.use(cors({
