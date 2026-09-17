8d51f2f1abd8defc64751c5e46b364a8b19c5626
real-Rahul1 <sahrahul04644@gmail.com>
2026-09-17 21:52:25 +0530

    added origins

--- server.js
+++ server.js
@@ -39,8 +39,9 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-    'https://cgeccse.in',
-    'https://cgeccse.in/techonicks-program'
+  'https://cgeccse.in',
+  'https://cgeccse.in/techonicks-program',
+  'https://cgeccse.in/techonicks-program/'
 ];
 
 app.use(cors({
