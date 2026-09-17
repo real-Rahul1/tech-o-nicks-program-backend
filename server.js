5efad90f41069a37d152c9d30651c060713bec98
real-Rahul1 <sahrahul04644@gmail.com>
2026-09-17 22:11:23 +0530

    added origins

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const allowedOrigins = [
   'https://tech-o-nicks-program.netlify.app',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'http://127.0.0.1:5501',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'https://cgeccse.in',
